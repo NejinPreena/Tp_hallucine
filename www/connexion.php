2ba82f1b51371f91f73f7072d238b864e2b4607c
NejinPreena <108547108+NejinPreena@users.noreply.github.com>
2022-08-19 11:22:22 -0400

    1er commit
    1er commit

---
+++ www/connexion.php
@@ -0,0 +1,39 @@
+<?php
+    try{
+        $database = connect("localhost", "hallucine", "root", "root");
+    }catch(Exception $error){
+        echo "Erreur de connexion à la BDD.<br>";
+        die("ERROR: ".$error->getMessage());
+    }
+    $sql = "SELECT * FROM `movies`;";
+    $results = $database->query($sql);
+    // var_dump($results->fetch(PDO::FETCH_BOTH));
+    // while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
+    //    echo $row["title"] . " " . $row["release_date"] . "<br>";
+    // }
+
+    $rows = $results->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($rows as $key => $value) {
+        echo $value["title"]. " " . $value["release_date"] ."<br>";
+    }
+
+    $sqlInsert = "INSERT INTO `movies` (`title`, `image_url`, `runtime`, `description`, `release_date`) VALUES (?, ?, ?, ?, ?)";
+
+    $request = $database->prepare($sqlInsert);
+
+    $title = "Je sais pas";
+    $imageUrl = "je_sais_pas.jpg";
+    $runtime = 10800;
+    $description = "Ceci est la desc.";
+    $releaseDate = "1887-10-12";
+
+    $request->execute([$title, $imageUrl, $runtime, $description, $releaseDate]);
+
+    $request->closeCursor();
+
+    function connect($host, $dbname, $login, $password){
+        return new PDO("mysql:host=".$host.";dbname=".$dbname, $login, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    
+?>
